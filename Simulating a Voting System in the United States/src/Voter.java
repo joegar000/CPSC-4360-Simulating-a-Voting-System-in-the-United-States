7d6f2e2b6b8a18ff547848e9c76e5adebb309750
Alejandro <alejandrofigueroa3183@gmail.com>
2021-04-01 15:34:52 -0500

    Adding properties

--- Simulating a Voting System in the United States/src/Voter.java
+++ Simulating a Voting System in the United States/src/Voter.java
@@ -7,12 +7,14 @@ public class Voter {
     public String DOB;
     public String State;
 
-    private String SSN;
-
     /*Example:
     Name: Joe Buden
     DOB: 03/16/1969
     State: Texas
     SSN (In some places it only requires numbers, while in other places it requires numbers and dashes.): 123 - 45 - 6789 or 123456789
     */
+    protected String SSN;
+    protected boolean ifVoted; 
+
+
 }
