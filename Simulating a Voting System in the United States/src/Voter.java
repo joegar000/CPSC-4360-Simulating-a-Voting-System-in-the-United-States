4d6ae6392ac0c45a4232391b33611f1462fe5640
Alejandro <alejandrofigueroa3183@gmail.com>
2021-04-01 15:29:57 -0500

    Comments

--- Simulating a Voting System in the United States/src/Voter.java
+++ Simulating a Voting System in the United States/src/Voter.java
@@ -8,4 +8,11 @@ public class Voter {
     public String State;
 
     private String SSN;
+
+    /*Example:
+    Name: Joe Buden
+    DOB: 03/16/1969
+    State: Texas
+    SSN (In some places it only requires numbers, while in other places it requires numbers and dashes.): 123 - 45 - 6789 or 123456789
+    */
 }
