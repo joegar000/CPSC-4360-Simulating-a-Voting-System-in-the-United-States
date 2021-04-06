8117fd243dfe1631c6134ec1791dcefb108d960e
Alejandro <alejandrofigueroa3183@gmail.com>
2021-04-06 12:28:46 -0500

    Updating Voter Class

--- Simulating a Voting System in the United States/src/Voter.java
+++ Simulating a Voting System in the United States/src/Voter.java
@@ -1,29 +1,46 @@
 /*
 Comments, edits, questions, suggestions, or concerns are welcomed!
 
-To do list:
-1. Unsure if to only check if the voter did vote or not. 
-Or check along side other factors such as voted, if they voted already, and if they are registered.
-Or separately check all three.
+Todo list:
+1. Discuss how to save the voters information and than check to see if the information matches. As well as, if they voted once.
+
 
 */
 
 public class Voter {
     /*Example:
     Name: Joe Buden
-    DOB: 03/16/1969
+    Age: 59
     State: Texas
     SSN (In some places it only requires numbers, 
     while in other places it requires numbers and dashes.): 123 - 45 - 6789 or 123456789
     Voted: No
     */
     private String VoterName;
-    private String VoterDOB;
+    private int VoterAge;
     private String VoterState;
     private String VoterSSN;
     private boolean ifVoted; 
     private boolean hasVoted;
     private boolean isRegistered;
+
+    //Default constructor
+    public Voter() {
+
+    }
+
+    //Constructor with the arguements.
+    public Voter(String VoterName, int VoterAge, 
+    String VoterState, String VoterSSN, boolean ifVoted, 
+    boolean hasVoted, boolean isRegistered) {
+        this.VoterName = VoterName;
+        this.VoterAge = VoterAge;
+        this.VoterState = VoterState;
+        this.ifVoted = ifVoted;
+        this.hasVoted = hasVoted;
+        this.isRegistered = isRegistered;
+
+    }
 
     //User either gets or sets the persons name.
     public void setVoterName(String newVoterName) {
@@ -33,12 +50,12 @@ public class Voter {
         return VoterName;
     }
 
-    //User either gets or sets the persons DOB.
-    public void setVoterDOB(String newVoterDOB) {
-        this.VoterDOB = newVoterDOB;
+    //User either gets or sets the persons Age.
+    public void setVoterAge(int newVoterAge) {
+        this.VoterAge = newVoterAge;
     }
-    public String getVoterDOB() {
-        return VoterDOB;
+    public int getVoterAge() {
+        return VoterAge;
     }
 
     //User either gets or sets the persons State.
@@ -57,6 +74,29 @@ public class Voter {
         return VoterSSN;
     }
 
-    //
+    /*
+    Note: This is a vague idea of doing it as it was 
+    unclear how to store the voters information and 
+    check to see if it matches. Unsure if the database 
+    that was mentioned would also do this.
+
+    Makeshift formula:
+    "Registering" = Name + Age + State + SSN
+    IsRegistered = "Registering"
+    IfVoted = IsRegistered 
+    HasVoted = IfVoted + IsRegistered
+
+    Each will be dependent on each other.
+    */
+    public boolean CheckIsRegistered() {
+
+    }
+    public boolean CheckIfVoted() {
+        
+    }
+    public boolean CheckHasVoted() {
+
+    }
+
 
 }
