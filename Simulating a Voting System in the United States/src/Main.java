019b48ca690f9d052fbbadaeb571e34d6ecc905a
joegar000 <69176505+joegar000@users.noreply.github.com>
2021-04-07 09:41:17 -0500

    Added Database Connectivity!!!

--- Simulating a Voting System in the United States/src/Main.java
+++ Simulating a Voting System in the United States/src/Main.java
@@ -3,10 +3,33 @@ Project: Voting System
 CPSC - 4360 - 01, Spring 2021
 
 */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 
 public class Main {
     public static void main(String[] args) throws Exception {
         
-        
+        Connection conn = null;
+        try {
+          String url = "jdbc:sqlite:chinook.db";
+          conn = DriverManager.getConnection(url);
+    
+          System.out.println("Got it!");
+    
+        } catch (SQLException e) {
+            throw new Error("Problem", e);
+        } finally {
+          try {
+            if (conn != null) {
+                conn.close();
+            }
+          } catch (SQLException ex) {
+              System.out.println(ex.getMessage());
+          }
+        }
+      }
     }
-}
--- Simulating a Voting System in the United States/src/PollWorker.java
+++ Simulating a Voting System in the United States/src/PollWorker.java
@@ -1,7 +1,17 @@
 /*
-
+David Garcia
 */
+// For JDBC
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class PollWorker {
     
+    public PollWorker() {
+        
+    }
+
 }
