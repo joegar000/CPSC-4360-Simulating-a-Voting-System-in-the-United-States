fc3e876b2bc8ef82aba160329361f6121788f158
joegar000 <69176505+joegar000@users.noreply.github.com>
2021-04-08 14:34:18 -0500

    Added database stuff

--- Simulating a Voting System in the United States/src/Main.java
+++ Simulating a Voting System in the United States/src/Main.java
@@ -9,27 +9,38 @@ import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import java.util.*;
 
+// David Garcia
+// COSC 1174
+// 3/19/2019
+// The purpose of this program is to display a 3 by 3 grid with the horizontal lines being blue and the vertical lines being red. The horizontal lines will always remain
+// 1/3 of the Scene's height from the side closest to them. Likewise, the vertical will always remain 1/3 of the Scene's width from the side closest to them.
+
+  
 public class Main {
-    public static void main(String[] args) throws Exception {
-        
-        Connection conn = null;
+  public static void main(String[] args) throws Exception {        
+
+
+
+      // Database stuff, we don't have to worry about this yet
+      Connection conn = null;
+      try {
+      String url = "jdbc:sqlite:chinook.db";
+      conn = DriverManager.getConnection(url);
+    
+      System.out.println("Got it!");
+    
+      } catch (SQLException e) {
+         throw new Error("Problem", e);
+      } finally {
         try {
-          String url = "jdbc:sqlite:chinook.db";
-          conn = DriverManager.getConnection(url);
-    
-          System.out.println("Got it!");
-    
-        } catch (SQLException e) {
-            throw new Error("Problem", e);
-        } finally {
-          try {
-            if (conn != null) {
-                conn.close();
-            }
-          } catch (SQLException ex) {
-              System.out.println(ex.getMessage());
+          if (conn != null) {
+              conn.close();
           }
-        }
-      }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+          }
     }
+  }
+}
--- Simulating a Voting System in the United States/src/PollWorker.java
+++ Simulating a Voting System in the United States/src/PollWorker.java
@@ -1,6 +1,7 @@
 /*
 David Garcia
 */
+
 // For JDBC
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -8,10 +9,52 @@ import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class PollWorker {
+public class PollWorker extends Voter {
     
-    public PollWorker() {
-        
+    // username and password is the same for all pollworkers
+    private String firstName;
+    private String lastName;
+    private String password = "1234";
+    private boolean loggedIn = false;
+
+    public PollWorker(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
+
+    /* If the username and password are incorrect, then try to log them in as a voter */
+    public void logIn(String firstName, String lastName, String password) {
+        if (this.password.equals(password)) {
+            this.loggedIn = true;
+        }
+        else {
+            System.out.println("The input name and/or password is incorrect.");
+        }
+    }
+
+    public void logOut() {
+        this.loggedIn = false;
+    }
+
+    public void displayVoterInfo() {
+        if (loggedIn == true) {
+            /* Pull data from database to display it */
+        }
+    }
+
+    public void registerVoter(String VoterName, int VoterAge, String VoterState, String VoterSSN, boolean ifVoted, boolean hasVoted, boolean isRegistered) {
+        Voter voter = new Voter(VoterName, VoterAge, VoterState, VoterSSN, ifVoted, hasVoted, isRegistered);
+        /* Add the voter object to the database */
+    }
+
+    public void printSuccess() {
+        System.out.println("it worked");
+    }
+
+
+
+
+
+
 
 }
