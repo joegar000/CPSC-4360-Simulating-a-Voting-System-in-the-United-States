ae29489319cded500f4a594ae115379d0ae1dd29
ahplasmind <ahplasmind@gmail.com>
2021-04-08 14:26:52 -0500

    added imports for sql

--- Simulating a Voting System in the United States/src/Administrator.java
+++ Simulating a Voting System in the United States/src/Administrator.java
@@ -9,6 +9,11 @@ TODO
     and display candidate database
 
 */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Administrator {
 
